14063a400d20b7fc38bdb6149d3af51ec8b5e30c
Chris Thorn <thorncp@gmail.com>
2014-01-12 14:53:14 -0900

    Refactor

--- irc.rs
+++ irc.rs
@@ -3,17 +3,13 @@ use std::io::net::addrinfo::get_host_addresses;
 
 fn main() {
   let ips = get_host_addresses("irc.freenode.net");
-  let mut ip: ~str;
 
-  match ips {
-    Some(vector) => ip = vector[0].to_str(),
+  let ip = match ips {
+    Some(vector) => vector[0],
     None => fail!("cannot get ips!")
-  }
+  };
 
-  let wat: Option<SocketAddr> = FromStr::from_str(format!("{:s}:6667", ip));
+  let server_address = SocketAddr { ip: ip, port: 6667 };
 
-  match wat {
-    Some(addr) => println(addr.to_str()),
-    None => println("no addr :(")
-  }
+  println(server_address.to_str());
 }
