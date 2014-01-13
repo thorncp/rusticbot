700eee9feca884f254d04710eee5adc489027465
Chris Thorn <thorncp@gmail.com>
2014-01-12 16:43:05 -0900

    Identify and send message to channel!

--- irc.rs
+++ irc.rs
@@ -1,10 +1,26 @@
 use std::io::net::ip::SocketAddr;
 use std::io::net::addrinfo::get_host_addresses;
 use std::io::net::tcp::TcpStream;
+use std::os;
 
 fn main() {
-  let server_address = match get_host_addresses("irc.freenode.net") {
-    Some(ips) => SocketAddr { ip: ips[0], port: 6667 },
+  let args = os::args();
+
+  if args.len() < 5 {
+    fail!("usage: ./irc <server> <port> <nick> <channel>");
+  }
+
+  // TODO figure out better args usage
+  let server = args[1].clone();
+  let port = match from_str::<u16>(args[2].clone()) {
+    Some(x) => x,
+    None => fail!("invalid port")
+  };
+  let nick = args[3].clone();
+  let channel = args[4].clone();
+
+  let server_address = match get_host_addresses(server) {
+    Some(ips) => SocketAddr { ip: ips[0], port: port },
     None => fail!("cannot get ip!")
   };
 
@@ -14,6 +30,11 @@ fn main() {
   };
 
   let mut buffer = [0u8, ..512];
+
+  stream.write_line(format!("USER {:s} {:s} {:s} {:s}", nick, nick, nick, nick));
+  stream.write_line(format!("NICK {:s}", nick));
+  stream.write_line(format!("JOIN {:s}", channel));
+  stream.write_line(format!("PRIVMSG {:s} :hello from rust!", channel));
 
   loop {
     match stream.read(buffer) {
